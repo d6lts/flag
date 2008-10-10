28e14e3653f52e4a273adac9354fddc3182169f1
Nathan Haug <quicksketch@35821.no-reply.drupal.org>
2008-10-10 06:52:34 +0000

    #313862 by mooffie. Fix and enhance the AJAX support. Addition of throbber image, removal of cached array, and reformatting to help consistency.

--- theme/flag.js
+++ theme/flag.js
@@ -1,119 +1,103 @@
 // $Id$
-if (Drupal.jsEnabled) {
-  $(document).ready(function () {
 
-    /**
-     * Flips a link. 'Flag this!' links turn into 'Unflag this!' and
-     * vice versa.
-     */
-    function flipLink(element, settings) {
-      // If this is a 'flag this' link...
-      if ($(element).is('.flag-action')) {
-        // ...then turn it into an 'unflag this' link;
-        var newHtml = settings.unflag;
-      }
-      else {
-        // else, turn it into a 'flag this' link.
-        var newHtml = settings.flag;
-      }
-      updateLink(element, newHtml, settings);
+/**
+ * Terminology:
+ *
+ *   "Link" means "Everything which is in flag.tpl.php" --and this may contain
+ *   much more than the <A> element. On the other hand, when we speak
+ *   specifically of the <A> element, we say "element" or "the <A> element".
+ */
+
+Drupal.flagLink = function(context) {
+  /**
+   * Helper function. Updates a link's HTML with a new one.
+   *
+   * @param element
+   *   The <A> element.
+   * @return
+   *   The new <A> elemenet.
+   */
+  function updateLink(element, newHtml) {
+    var $newLink = $(newHtml);
+
+    // Initially hide the message so we can fade it in.
+    $('.flag-message', $newLink).css('display', 'none');
+
+    // Reattach the behavior to the new <A> element. This element
+    // is either whithin the wrapper or it is the outer element itself.
+    var $nucleus = $newLink.is('a') ? $newLink : $('a.flag', $newLink);
+    $nucleus.addClass('flag-processed').click(flagClick);
+
+    // Find the wrapper of the old link.
+    var $wrapper = $(element).parents('.flag-wrapper:first');
+    if ($wrapper.length == 0) {
+      // If no ancestor wrapper was found, or if the 'flag-wrapper' class is
+      // attached to the <a> element itself, then take the element itself.
+      $wrapper = $(element);
+    }
+    // Replace the old link with the new one.
+    $wrapper.after($newLink).remove();
+
+    // So it won't crash on Drupal 5.
+    if (Drupal.attachBehaviors) {
+      Drupal.attachBehaviors($newLink.get(0));
     }
 
-    /**
-     * Helper function. Updates a link's HTML with a new one.
-     */
-    function updateLink(element, newHtml, settings) {
-      var $newLink = $(newHtml);
+    $('.flag-message', $newLink).fadeIn();
+  }
+  
+  /**
+   * A click handler that is attached to all <A class="flag"> elements.
+   */
+  function flagClick() {
+    // 'this' won't point to the element when it's inside the ajax closures,
+    // so we reference it using a variable.
+    var element = this;
 
-      // Initially hide the message so we can fade it in.
-      $('.flag-message', $newLink).css('display', 'none');
-
-      // Reattach the behavior to the new link.
-      if ($('a', $newLink).size() > 0) {
-        $('a', $newLink).bind('click', function() { return flagClick(this, settings) });
-      }
-      else {
-        $newLink.bind('click', function() { return flagClick(this, settings) });
-      }
-
-      // Find the wrapper of the old link.
-      var $wrapper = $(element).parents('.flag-wrapper:first');
-      if ($wrapper.length == 0) {
-        // If no ancestor wrapper was found, or if the 'flag-wrapper' class is
-        // attached to the <a> element itself, then take the element itself.
-        $wrapper = $(element);
-      }
-      // Replace the old link with the new one.
-      $wrapper.after($newLink).remove();
-
-      $('.flag-message', $newLink).fadeIn();
-    }
-    
-    // Click function for each Flag link.
-    function flagClick(element, settings) {
-      // Hide any other active messages.
-      $('span.flag-message:visible').fadeOut();
-
-      // Send POST request
-      $.ajax({
-        type: 'POST',
-        url: element.href,
-        data: { js: true },
-        dataType: 'json',
-        success: function (data) {
-          // Display errors
-          if (!data.status) {
-            // Change link back
-            flipLink(element, settings);
-            return;
-          }
-        },
-        error: function (xmlhttp) {
-          alert('An HTTP error '+ xmlhttp.status +' occurred.\n'+ element.href);
-          // Change link back
-          flipLink(element, settings);
-        }
-      });
-      // Swap out the links.
-      flipLink(element, settings);
+    // While waiting for a server response, the wrapper will have a
+    // 'flag-waiting' class. Themers are thus able to style the link
+    // differently, e.g., by displaying a throbber.
+    var $wrapper = $(element).parents('.flag-wrapper');
+    if ($wrapper.is('.flag-waiting')) {
+      // Guard against double-clicks.
       return false;
     }
+    $wrapper.addClass('flag-waiting');
 
-    /**
-     * Like alert(), but displays only once, to keep user from losing sanity.
-     */
-    var warn = function(message) {
-      if (!Drupal.settings.flag.alertShown) {
-        alert('Flag module: ' + message);
-        Drupal.settings.flag.alertShown = true;
+    // Hide any other active messages.
+    $('span.flag-message:visible').fadeOut();
+
+    // Send POST request
+    $.ajax({
+      type: 'POST',
+      url: element.href,
+      data: { js: true },
+      dataType: 'json',
+      success: function (data) {
+        if (data.status) {
+          // Success.
+          updateLink(element, data.newLink);
+        }
+        else {
+          // Failure.
+          alert(data.errorMessage);
+          $wrapper.removeClass('flag-waiting');
+        }
+      },
+      error: function (xmlhttp) {
+        alert('An HTTP error '+ xmlhttp.status +' occurred.\n'+ element.href);
       }
-    }
-
-    /**
-     * Returns the settings for a certain link element.
-     */
-    function getLinkSettings(element) {
-      // The link URL is of the form ?q=flag/unflag/bookmarks/23,
-      // so let's parse it to extract the flag name and the content ID.
-      var matches = element.href.match(/flag\/(un)?flag\/(\w+)\/(\d+)/);
-      if (!matches) {
-        warn("Error: Invalid flag URL '" + element.href + "'");
-      }
-      var flagName  = matches[2];
-      var contentId = matches[3];
-      var slot = 'cid_' + contentId;
-
-      if (!Drupal.settings.flag.flags[flagName][slot]) {
-        // Slot does not exist. Create.
-        Drupal.settings.flag.flags[flagName][slot] = {};
-      }
-      // Return a reference to the settings slot.
-      return Drupal.settings.flag.flags[flagName][slot];
-    }
-
-    // On load, bind the click behavior for all links on the page.
-    $('a.flag').click(function() {
-      return flagClick(this, getLinkSettings(this));
     });
+    return false;
+  }
+
+  $('a.flag:not(.flag-processed)').addClass('flag-processed').click(flagClick);
+};
+
+
+if (Drupal.jsEnabled) {
+  $(document).ready(function(){
+    // On load, bind the click behavior for all links on the page.
+    Drupal.flagLink(document);
   });
 }
