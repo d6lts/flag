ed31b76434b2c982100b2b3a3d2de043dcecdb30
Nathan Haug <quicksketch@35821.no-reply.drupal.org>
2008-11-26 22:51:39 +0000

    #337645. Coder style fixes. Mostly whitespace and punctuation changes.

--- theme/flag.tpl.php
+++ theme/flag.tpl.php
@@ -3,7 +3,7 @@
 
 /**
  * @file flag.tpl.php
- * Default theme implementation to display a flag link, and a message after the action 
+ * Default theme implementation to display a flag link, and a message after the action
  * is carried out.
  *
  * Available variables:
@@ -15,7 +15,7 @@
  * - $flag_classes: A space-separated list of CSS classes that should be applied to the link.
  *
  * - $action: The action the link is about to carry out, either "flag" or "unflag".
- * - $last_action: The action, as a passive English verb, either "flagged" or 
+ * - $last_action: The action, as a passive English verb, either "flagged" or
  *   "unflagged", that led to the current status of the flag.
  *
  * - $link_href: The URL for the flag link.
@@ -27,8 +27,8 @@
  *   flagged. If displaying to the user immediately after flagging, this value
  *   will be boolean TRUE. This is usually used in conjunction with immedate
  *   JavaScript-based toggling of flags.
- * - $setup: TRUE when this template is parsed for the first time; Use this 
- *   flag to carry out procedures that are needed only once; e.g., linking to CSS 
+ * - $setup: TRUE when this template is parsed for the first time; Use this
+ *   flag to carry out procedures that are needed only once; e.g., linking to CSS
  *   and JS files.
  *
  * NOTE: This template spaces out the <span> tags for clarity only. When doing some
