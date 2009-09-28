8309013f7ae7c2109832966de8c1c7308d00f55d
Nathan Haug <quicksketch@35821.no-reply.drupal.org>
2009-09-28 02:05:06 +0000

    #285237: Ability to disallow a flag/unflag operation. Adds separate permissions for flag/unflag operations. Restructures flag form.

--- theme/flag.tpl.php
+++ theme/flag.tpl.php
@@ -40,11 +40,15 @@
     drupal_add_js(drupal_get_path('module', 'flag') .'/theme/flag.js');
   }
 ?>
-<span class="flag-wrapper flag-<?php echo $flag_name_css; ?>">
-  <a href="<?php echo $link_href; ?>" title="<?php echo $link_title; ?>" class="<?php print $flag_classes ?>" rel="nofollow"><?php echo $link_text; ?></a><span class="flag-throbber">&nbsp;</span>
+<span class="flag-wrapper flag-<?php print $flag_name_css; ?>">
+  <?php if ($link_href): ?>
+    <a href="<?php print $link_href; ?>" title="<?php print $link_title; ?>" class="<?php print $flag_classes ?>" rel="nofollow"><?php print $link_text; ?></a><span class="flag-throbber">&nbsp;</span>
+  <?php else: ?>
+    <span class="<?php print $flag_classes ?>"><?php print $link_text; ?></span>
+  <?php endif; ?>
   <?php if ($after_flagging): ?>
-    <span class="flag-message flag-<?php echo $last_action; ?>-message">
-      <?php echo $message_text; ?>
+    <span class="flag-message flag-<?php print $last_action; ?>-message">
+      <?php print $message_text; ?>
     </span>
   <?php endif; ?>
 </span>
